d8b5043098bb151799547d913feaefc61de33b1d
subratahazra985 <subratahazra985@gmail.com>
2025-06-27 01:20:36 +0530

    Remove deprecated code in generate token method and add logger to the console with different color

--- src/main/java/com/subro/blog/entities/User.java
+++ src/main/java/com/subro/blog/entities/User.java
@@ -36,6 +36,15 @@ public class User implements UserDetails {
    inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "id"))
    private Set<Role> roles=new HashSet<>();
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>This implementation returns a list of {@link SimpleGrantedAuthority} objects, where each
+     * authority is the name of the role that the user has. The roles are obtained from the
+     * {@link #roles} field.
+     *
+     * @return a list of {@link SimpleGrantedAuthority} objects
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         // iterate each role and change it to granted authority
@@ -43,26 +52,64 @@ public class User implements UserDetails {
         return authorities;
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * The username is equal to the user's email address.
+     * @return the user's email address
+     */
     @Override
     public String getUsername() {
         return this.email;
     }
 
+    /**
+     * Indicates whether the user's account is non-expired.
+     *
+     * <p>An expired account cannot be authenticated. This method delegates to the
+     * default implementation provided by the {@link UserDetails} interface.
+     *
+     * @return {@code true} if the user's account is non-expired, {@code false} otherwise
+     */
     @Override
     public boolean isAccountNonExpired() {
         return UserDetails.super.isAccountNonExpired();
     }
 
+    /**
+     * Indicates whether the user's account is non-locked.
+     *
+     * <p>A locked account cannot be authenticated. This method delegates to the
+     * default implementation provided by the {@link UserDetails} interface.
+     *
+     * @return {@code true} if the user's account is non-locked, {@code false} otherwise
+     */
     @Override
     public boolean isAccountNonLocked() {
         return UserDetails.super.isAccountNonLocked();
     }
 
+    /**
+     * Indicates whether the user's credentials are non-expired.
+     *
+     * <p>An expired credential cannot be authenticated. This method delegates to the
+     * default implementation provided by the {@link UserDetails} interface.
+     *
+     * @return {@code true} if the user's credentials are non-expired, {@code false} otherwise
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return UserDetails.super.isCredentialsNonExpired();
     }
 
+    /**
+     * Indicates whether the user's account is enabled.
+     *
+     * <p>An enabled account can be authenticated and used normally within the system.
+     * This method always returns {@code true}, indicating that the account is enabled by default.
+     *
+     * @return {@code true} if the user's account is enabled, {@code false} otherwise
+     */
     @Override
     public boolean isEnabled() {
         return true;
